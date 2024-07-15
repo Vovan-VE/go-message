dc96a3f7938245d1a93db0a886d6e0591f1f0449
Vovan-VE <vovan-ve@yandex.ru>
2024-07-15 13:26:06 +0800

    Add unsafe test for message.DecodeTextAttachments option

--- mail/reader_test.go
+++ mail/reader_test.go
@@ -1,13 +1,18 @@
 package mail_test
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
+	"mime/quotedprintable"
 	"strings"
 	"testing"
 
+	"github.com/emersion/go-message"
+	_ "github.com/emersion/go-message/charset"
 	"github.com/emersion/go-message/mail"
+	"golang.org/x/text/encoding/charmap"
 )
 
 func ExampleReader() {
@@ -200,5 +205,95 @@ func TestReader_nested(t *testing.T) {
 		}
 
 		i++
+	}
+}
+
+func TestRead_NoDecodeTextAttachment(t *testing.T) {
+	const attachmentContentType = "text/plain; charset=windows-1251"
+	const attachmentBodyUtf8 = "А-ЯЁа-яё№"
+	attachmentBodyRaw, err := charmap.Windows1251.NewEncoder().String(attachmentBodyUtf8)
+	if err != nil {
+		t.Fatal("encode attach body", err)
+	}
+	if len([]byte(attachmentBodyRaw)) != 9 {
+		t.Fatal("incorrect encode:", []byte(attachmentBodyRaw))
+	}
+	attachmentBodyQP := new(strings.Builder)
+	qpw := quotedprintable.NewWriter(attachmentBodyQP)
+	qpw.Write([]byte(attachmentBodyRaw))
+	qpw.Close()
+	if attachmentBodyQP.Len() != 23 {
+		t.Fatal("unexpected QP:", attachmentBodyQP.String())
+	}
+
+	inputMiltipart := "Mime-Version: 1.0\r\n" +
+		"Content-Type: multipart/mixed; boundary=IMTHEBOUNDARY\r\n" +
+		"\r\n" +
+		"--IMTHEBOUNDARY\r\n" +
+		"Content-Type: text/plain\r\n" +
+		"\r\n" +
+		"Text part\r\n" +
+		"--IMTHEBOUNDARY\r\n" +
+		"Content-Disposition: attachment; filename=cp1251.txt\r\n" +
+		"Content-Type: " + attachmentContentType + "\r\n" +
+		"Content-Transfer-Encoding: quoted-printable\r\n" +
+		"\r\n" +
+		attachmentBodyQP.String() + "\r\n" +
+		"--IMTHEBOUNDARY--\r\n"
+
+	for _, option := range []bool{true, false} {
+		t.Run(fmt.Sprintf("DecodeTextAttachments = %v", option), func(t *testing.T) {
+			optOrig := message.DecodeTextAttachments
+			message.DecodeTextAttachments = option
+			defer func() {
+				message.DecodeTextAttachments = optOrig
+			}()
+
+			mr, err := mail.CreateReader(strings.NewReader(inputMiltipart))
+			if err != nil {
+				t.Fatal("unexpected error: ", err)
+			}
+			defer mr.Close()
+
+			i := 0
+			for {
+				p, err := mr.NextPart()
+				if err == io.EOF {
+					break
+				} else if err != nil {
+					t.Fatal("Expected no error while reading multipart entity, got", err)
+				}
+
+				var expectedType string
+				var expectedBody string
+				switch i {
+				case 0:
+					expectedType = "text/plain"
+					expectedBody = "Text part"
+				case 1:
+					expectedType = attachmentContentType
+					if option {
+						expectedBody = attachmentBodyUtf8
+					} else {
+						expectedBody = attachmentBodyRaw
+					}
+				}
+
+				if mediaType := p.Header.Get("Content-Type"); mediaType != expectedType {
+					t.Errorf("Expected part Content-Type to be %q, got %q", expectedType, mediaType)
+				}
+				if b, err := ioutil.ReadAll(p.Body); err != nil {
+					t.Error("Expected no error while reading part body, got", err)
+				} else if s := string(b); s != expectedBody {
+					t.Errorf("Expected %q as part body but got %q", expectedBody, s)
+				}
+
+				i++
+			}
+
+			if i != 2 {
+				t.Fatalf("Expected multipart entity to contain exactly 2 parts, got %v", i)
+			}
+		})
 	}
 }
